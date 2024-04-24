1e3e35afcd1150746bab960f4c4f46c1c810c55d
MarcoRivasTec <160761723+MarcoRivasTec@users.noreply.github.com>
2024-04-24 10:59:18 -0600

    Added picture to card

--- components/Home/Card.jsx
+++ components/Home/Card.jsx
@@ -1,12 +1,32 @@
-import React from "react";
-import { View, Text } from "react-native";
+import React, { useState, useEffect } from "react";
+import { View, Text, Image } from "react-native";
 import { LinearGradient } from "expo-linear-gradient";
 import { data } from "./styles";
-import Svg, { Circle } from "react-native-svg";
+// import Svg, { Circle } from "react-native-svg";
 import Icon from "./icons";
 import COLORS from "../../constants/colors";
 
 function Card() {
+	// const [photoExists, setPhotoExists] = useState(require("../../assets/social/imagen.png"));
+
+	// useEffect(() => {
+	// 	const getImage = async () => {
+	// 		const imageUrl = "../../assets/social/imagen.png"; // Image path
+	// 		try {
+	// 			const resolvedImage = Image.resolveAssetSource(imageUrl);
+	// 			if (resolvedImage) {
+	// 				setPhotoExists(resolvedImage.uri); // Set image URI to state
+	// 			} else {
+	// 				setPhotoExists(null); // If image not found, set image URI to null
+	// 			}
+	// 		} catch (error) {
+	// 			console.error("Error resolving image:", error);
+	// 		}
+	// 	};
+
+	// 	getImage();
+	// }, []);
+
 	return (
 		<View style={data.container}>
 			{/* Flex de 10 */}
@@ -32,23 +52,25 @@ function Card() {
 					start={{ x: 0, y: 0 }}
 					end={{ x: 1, y: 1 }}
 				></LinearGradient>
-				{/* <SVGBanner style = {{flex: 1}}>
-                    </SVGBanner> */}
 				<View style={data.cardDataContainer}>
 					<View style={data.cardAvatarContainer}>
 						<View style={data.cardAvatarBackground}>
-							<Icon
-								name="USER"
-								size={36}
-								color="gray"
-							></Icon>
+							{/* {photoExists ? ( */}
+								<Image
+									style={data.image}
+									resizeMode="contain"
+									source={require("../../assets/social/imagen.png")}
+								/>
+							{/* ) : ( */}
+								{/* <Icon name="USER" size={36} color="gray"></Icon> */}
+							{/* )} */}
 						</View>
 					</View>
 
-					{/* <View style = {{borderRadius: 50, padding: "12.5%", backgroundColor: COLORS.white}}> 
-                        </View> */}
 					<View style={data.cardTextContainer}>
-						<Text style={[data.cardText, { fontSize: 24 }]}>Hello, Marcos!</Text>
+						<Text style={[data.cardText, { fontSize: 24 }]}>
+							Hello, Marcos!
+						</Text>
 						<Text style={[data.cardText, { fontSize: 16, fontWeight: "bold" }]}>
 							94327
 						</Text>
