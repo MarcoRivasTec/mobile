83b4e56ba29b6db7b2492388158e0c8a2e795218
Zenith <arcarielle@hotmail.com>
2024-10-10 08:56:32 -0600

    Working modal

---
+++ components/Home/Content/Design/Working.jsx
@@ -0,0 +1,41 @@
+import React, { useEffect } from "react";
+import { Modal, View, Text } from "react-native";
+import { working } from "./styles";
+import LoadingContent from "../../../Animations/LoadingContent";
+
+function Working({
+	isModalVisible,
+	onCallback,
+	onExit,
+}) {
+	return (
+		<View style={working.container}>
+			<Modal
+				animationType="fade"
+				transparent={true}
+				visible={isModalVisible}
+				onRequestClose={onCallback}
+			>
+				<View style={working.backgroundContainer}>
+					<View style={working.modalContainer}>
+						<View style={working.contentContainer}>
+							{/* Title */}
+							<View style={working.animContainer}>
+								<View style={working.anim}>
+									<LoadingContent />
+								</View>
+							</View>
+
+							{/* Text */}
+							<Text style={working.text}>
+								"Procesando ..."
+							</Text>
+						</View>
+					</View>
+				</View>
+			</Modal>
+		</View>
+	);
+}
+
+export default Working;
--- components/Home/Content/Design/styles.jsx
+++ components/Home/Content/Design/styles.jsx
@@ -61,4 +61,58 @@ const confirm = StyleSheet.create({
 	},
 });
 
-export { confirm };
+const working = StyleSheet.create({
+	container: {
+		flex: 1,
+	},
+	backgroundContainer: {
+		flex: 1,
+		width: "100%",
+		justifyContent: "center",
+		alignItems: "center",
+		// backgroundColor: "rgba(255,255,255, 0.9)",
+		backgroundColor: "rgba(0, 0, 0, 0.4)",
+	},
+	modalContainer: {
+		height: "auto",
+		paddingVertical: "6%",
+		width: "70%",
+		justifyContent: "center",
+		alignItems: "center",
+		backgroundColor: "rgba(242, 242, 242, 1)",
+		borderRadius: 25,
+	},
+	contentContainer: {
+		// flex: 1,
+		height: "auto",
+		width: "90%",
+		justifyContent: "center",
+		alignItems: "center",
+	},
+	animContainer: {
+		// flex: 2.5,
+		height: 80,
+        width: "100%",
+		marginTop: "4%",
+		// borderWidth: 1,
+		justifyContent: "center",
+		alignItems: "center",
+	},
+    anim: {
+        width: "100%",
+        height: "100%",
+		// borderWidth: 1,
+		// borderRadius: 10,
+		justifyContent: "center",
+		alignItems: "center",
+    },
+	text: {
+		marginTop: "4%",
+		fontSize: 22,
+		fontWeight: "500",
+		color: COLORS.black,
+        textAlign: "center",
+	},
+});
+
+export { confirm, working };
