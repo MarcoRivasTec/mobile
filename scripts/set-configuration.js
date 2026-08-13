cf4984bbfe651518e1d977aa1898e9003b68f6d5
Marco Rivas <marco.rivas@tecma.com>
2026-08-13 13:39:29 -0600

    Update app version to 1.1.7 and adjust development version in set-configuration script

--- scripts/set-configuration.js
+++ scripts/set-configuration.js
@@ -31,7 +31,7 @@ async function updateJsonValues(mode) {
 		if (mode === "dev") {
 			appJson.expo.name = "TECMA Móvil Connect Dev";
 			appJson.expo.slug = "tecma-movil-connect-dev";
-			appJson.expo.version = "1.1.6dev";
+			appJson.expo.version = "1.1.7dev";
 			appJson.expo.icon = "./assets/icon-dev.png";
 			appJson.expo.splash.image = "./assets/icon-dev.png";
 			appJson.expo.android.adaptiveIcon.foregroundImage = "./assets/icon-dev.png";
@@ -45,7 +45,7 @@ async function updateJsonValues(mode) {
 			
 			appJson.expo.name = "TECMA Móvil Connect";
 			appJson.expo.slug = "tecma-movil-connect";
-			appJson.expo.version = "1.1.6";
+			appJson.expo.version = "1.1.7";
 			appJson.expo.icon = "./assets/icon.png";
 			appJson.expo.splash.image = "./assets/icon.png";
 			appJson.expo.android.adaptiveIcon.foregroundImage = "./assets/adaptive-icon.png";
